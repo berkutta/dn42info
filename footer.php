549d528ae4a1b781dde0c8de9706ff0caaaddd0b
Benjamin Marty <info@benjaminmarty.ch>
2018-12-20 19:35:21 +0100

    sdfsdf

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
 	</div>
+
+	<footer class="footer">
+      <div class="container">
+        <span class="text-muted">Fork: <a href="https://github.com/berkutta/dn42info">Link</a>, Original: <a href="https://github.com/manawyrm/dn42info">Link</a></span>
+      </div>
+    </footer>
+    
 </body>
 </html>
